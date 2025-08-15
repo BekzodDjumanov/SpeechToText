b0fb9bdd82c8306dc05d869019759d84137c66e7
BekzodDjumanov <bdjumanov1@gmail.com>
2025-08-14 22:21:50 -0400

    New intro-loader

--- static/main.js
+++ static/main.js
@@ -1,47 +1,43 @@
-document.getElementById('upload-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+document
+  .getElementById("upload-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const loadingOverlay = document.getElementById('loading-overlay');
-  loadingOverlay.style.display = 'flex'; // set to flex to make it visible
+    const loadingOverlay = document.getElementById("loading-overlay");
+    loadingOverlay.style.display = "flex"; // set to flex to make it visible
 
-  // GSAP animation
-  gsap.fromTo(loadingOverlay, 
-    { opacity: 0, y: "-300" },
-    {
-      opacity: 1, 
-      y: "0", 
-      duration: 2, 
-      ease: 'power2.out',
-      onStart: function() {
-        // prevent submitting multiple requests
-        document.querySelector('button[type="submit"]').disabled = true;
-      },
-      onComplete: () => {
-        // re-enable inbound requests
-        document.querySelector('button[type="submit"]').disabled = false;
+    // GSAP animation
+    gsap.fromTo(
+      loadingOverlay,
+      { opacity: 0, y: "-300" },
+      {
+        opacity: 1,
+        y: "0",
+        duration: 2,
+        ease: "power2.out",
+        onStart: function () {
+          // prevent submitting multiple requests
+          document.querySelector('button[type="submit"]').disabled = true;
+        },
+        onComplete: () => {
+          // re-enable inbound requests
+          document.querySelector('button[type="submit"]').disabled = false;
 
-        this.submit(); // trigger form submission
+          this.submit(); // trigger form submission
+        },
       }
-    }
-  );
+    );
+  });
+
+document.querySelector(".dark-light-btn").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
 
-document.querySelector('.dark-light-btn').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-  
 // library for movement
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true,
-    once: true
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
+  once: true,
 });
-
-
-sr.reveal('.header', {origin: 'top',reset: false});
-sr.reveal('.hero-card', {delay: 500, origin: 'bottom',reset: false});
-sr.reveal('.dark-light-btn', {origin: 'left',reset: false});
-
-  
